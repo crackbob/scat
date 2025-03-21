cb0010172652b782d16571ef69772186a4887633
crackbob <crackbob.999@gmail.com>
2025-03-21 08:34:07 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+let wpRequire;
+webpackChunkGUI.push([[Symbol()], {}, function (require) {wpRequire = require}]);
+let vm = wpRequire(Object.keys(wpRequire.m)[Object.values(wpRequire.m).findIndex(m => m.toString().includes("guiInitialState="))]).guiInitialState.vm;
+
+function spoofUsername (name) {
+    vm.runtime._primitives.sensing_username = () => name;
+    vm.runtime.ioDevices.userData._username = name;
+}
+
+["hasCloudPermission", "canModifyCloudData"].forEach(prop => 
+    Object.defineProperty(Object.prototype, prop, {
+        get: () => true,
+        set: () => {},
+        enumerable: false
+    })
+);

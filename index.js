c32fb391cc5f4d64c1ef453fa0dfb6a186ccfa8a
crackbob <crackbob.999@gmail.com>
2025-03-21 08:40:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let wpRequire;
-webpackChunkGUI.push([[Symbol()], {}, function (require) {wpRequire = require}]);
-let vm = wpRequire(Object.keys(wpRequire.m)[Object.values(wpRequire.m).findIndex(m => m.toString().includes("guiInitialState="))]).guiInitialState.vm;
-
 function spoofUsername (name) {
+    let wpRequire;
+    webpackChunkGUI.push([[Symbol()], {}, function (require) {wpRequire = require}]);
+    let vm = wpRequire(Object.keys(wpRequire.m)[Object.values(wpRequire.m).findIndex(m => m.toString().includes("guiInitialState="))]).guiInitialState.vm;
     vm.runtime._primitives.sensing_username = () => name;
     vm.runtime.ioDevices.userData._username = name;
 }
